b548e9f216e8cec0355ac752c1ba42ea2a0dd619
sahil-au7 <sahilyadav601@gmail.com>
2020-09-10 00:04:15 +0530

    added burger controls

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,12 @@
-import React from 'react';
+import React, { Fragment } from "react";
 
-import Aux from '../../hoc/Aux';
-import classes from './Layout.css';
+import classes from "./Layout.css";
 
-const layout = ( props ) => (
-    <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
+const layout = (props) => (
+  <Fragment>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
+  </Fragment>
 );
 
 export default layout;
